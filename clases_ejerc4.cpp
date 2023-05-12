f5da9c4debb522d7013e32e7fc6ef18b8ebf65d8
AyaqueCallaRodrigo <132325295+AyaqueCallaRodrigo@users.noreply.github.com>
2023-05-12 11:06:34 -0500

    Update clases_ejerc4.cpp

--- clases_ejerc4.cpp
+++ clases_ejerc4.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <string>
 
-using namespace std;                                                                                                                                                                       // Clase para representar un vector en 2D
-
+// Clase para representar un vector en 2D
 class Vector2D {
 public:
-
     // Constructores
-
     Vector2D() : x(0.0), y(0.0) {}
     Vector2D(double xx, double yy) : x(xx), y(yy) {}
 
     // Operaciones de vectores
-
     Vector2D operator+(const Vector2D& v) const {
         return Vector2D(x + v.x, y + v.y);
     }
@@ -21,26 +17,20 @@ public:
         return Vector2D(x - v.x, y - v.y);
     }
 
-    // Sobrecarga de operadores
-    friend Vector2D operator+(const Vector2D& v1, const Vector2D& v2) {
-        return v1 + v2;
-
-    }
-
-
-
-    friend Vector2D operator-(const Vector2D& v1, const Vector2D& v2) {
-        return v1 - v2;
-
-    }
-
     // Variables miembro
     double x, y;
-
 };
 
-int main() {
+// Sobrecarga de operadores fuera de la clase
+Vector2D operator+(const Vector2D& v1, const Vector2D& v2) {
+    return Vector2D(v1.x + v2.x, v1.y + v2.y);
+}
 
+Vector2D operator-(const Vector2D& v1, const Vector2D& v2) {
+    return Vector2D(v1.x - v2.x, v1.y - v2.y);
+}
+
+int main() {
     // Crear dos vectores
     Vector2D v1(1.0, 2.0);
     Vector2D v2(3.0, 4.0);
@@ -50,13 +40,10 @@ int main() {
     Vector2D resta = v1 - v2;
 
     // Imprimir los resultados
-    cout << "v1 = (" << v1.x << ", " << v1.y << ")" << std::endl;
-    cout << "v2 = (" << v2.x << ", " << v2.y << ")" << std::endl;
-    cout << "v1 + v2 = (" << suma.x << ", " << suma.y << ")" << std::endl;
-    cout << "v1 - v2 = (" << resta.x << ", " << resta.y << ")" << std::endl;
-
-
+    std::cout << "v1 = (" << v1.x << ", " << v1.y << ")" << std::endl;
+    std::cout << "v2 = (" << v2.x << ", " << v2.y << ")" << std::endl;
+    std::cout << "v1 + v2 = (" << suma.x << ", " << suma.y << ")" << std::endl;
+    std::cout << "v1 - v2 = (" << resta.x << ", " << resta.y << ")" << std::endl;
 
     return 0;
-
 }
